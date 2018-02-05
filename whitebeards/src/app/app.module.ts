1bfe299d0134738490450d0c854c021f264b8957
Stirling Greensides <stirling.greensides@dhltd.com>
2018-02-05 10:44:51 -0500

    Start of best practices, proper file naming.

---
+++ whitebeards/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
+
+import { appRoutes } from './routes'
+import { AppComponent }  from './components/app.component';
+import { NavBarComponent }  from './components/nav-bar.component';
+import { CatalogComponent } from "./components/catalog.component";
+import { RegisterComponent } from "./components/sign-in.component";
+import { SignInComponent } from "./components/sign-in.component";
+import { LoadingComponent } from "./components/loading-spinner.component";
+import { DataRepositoryService } from "./services/data-repository"
+import { AccountMenuComponent } from "./components/account-menu.component";
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CatalogComponent,
+    RegisterComponent,
+    SignInComponent,
+    LoadingComponent,
+    AccountMenuComponent
+  ],
+  providers: [ DataRepositoryService ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
